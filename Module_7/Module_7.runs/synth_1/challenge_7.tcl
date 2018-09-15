3635593208095bbd0c82616cfd0f27a5351a9a73
Elliott Villars <eman13vil@gmail.com>
2018-09-15 12:25:18 -0700

    -

--- Module_7/Module_7.runs/synth_1/challenge_7.tcl
+++ Module_7/Module_7.runs/synth_1/challenge_7.tcl
@@ -17,6 +17,7 @@ proc create_report { reportName command } {
     send_msg_id runtcl-5 warning "$msg"
   }
 }
+set_msg_config -id {Common 17-41} -limit 10000000
 create_project -in_memory -part xc7z007sclg400-1
 
 set_param project.singleFileAddWarning.threshold 0
@@ -41,6 +42,9 @@ read_verilog -library xil_defaultlib {
 foreach dcp [get_files -quiet -all -filter file_type=="Design\ Checkpoint"] {
   set_property used_in_implementation false $dcp
 }
+read_xdc C:/Users/Sergaljerk/WSU-CPTE/Module_7/Module_7.srcs/constrs_1/new/challange_7_cons.xdc
+set_property used_in_implementation false [get_files C:/Users/Sergaljerk/WSU-CPTE/Module_7/Module_7.srcs/constrs_1/new/challange_7_cons.xdc]
+
 set_param ips.enableIPCacheLiteLoad 0
 close [open __synthesis_is_running__ w]
 
