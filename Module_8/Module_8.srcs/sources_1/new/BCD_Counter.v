a544891f791a3b63e8400ce02cf924b4f46544fa
Elliott Villars <eman13vil@gmail.com>
2018-10-13 23:47:08 -0700

    -

--- Module_8/Module_8.srcs/sources_1/new/BCD_Counter.v
+++ Module_8/Module_8.srcs/sources_1/new/BCD_Counter.v
@@ -47,64 +47,45 @@ module BCD_Counter(
     assign o_Anodes = w_ANODE;
     //assign o_Anodes = 4'b0111;
     
-    HZ_Counter #(.c_NUM(50000000)) Digit_1(
-    .i_CLK(w_CLK),
-    .i_RST(w_RST),
-    .o_Out(w_SUBCLK_1HZ)
-    );
-    HZ_Counter #(.c_NUM(5000000)) Digit_2(
-    .i_CLK(w_CLK),
-    .i_RST(w_RST),
-    .o_Out(w_SUBCLK_10HZ)
-     );
-     HZ_Counter #(.c_NUM(500000)) Digit_3(
-     .i_CLK(w_CLK),
-     .i_RST(w_RST),
-     .o_Out(w_SUBCLK_100HZ)
-      );
-     HZ_Counter #(.c_NUM(50000)) Digit_4(
-     .i_CLK(w_CLK),
-     .i_RST(w_RST),
-     .o_Out(w_SUBCLK_1KHZ)
-      );
+
       
-        HZ_Counter #(.c_NUM(5000)) SVD_CLK(
+        HZ_Counter #(.c_NUM(50000)) SVD_CLK(
          .i_CLK(w_CLK),
          .i_RST(w_RST),
-         .o_Out(w_SUBCLK_10KHZ)
+         .o_Out(w_SUBCLK_1KHZ)
           );
+          HZ_Counter #(.c_NUM(5000)) SVD_CLK_1(
+                   .i_CLK(w_CLK),
+                   .i_RST(w_RST),
+                   .o_Out(w_SUBCLK_10KHZ)
+                    );
     
-    always@(posedge w_SUBCLK_1HZ)
-    begin
-        if(r_Digit_1_val == 4'hA)
-            r_Digit_1_val <=4'd0;
+
+    always@(posedge w_SUBCLK_1KHZ) //move to own module
+    begin  
+        if(r_Digit_4_val >= 4'd9)
+        begin
+            r_Digit_4_val <= 4'd0;
+            if(r_Digit_3_val >= 4'd9)
+            begin
+                r_Digit_3_val <= 4'd0;
+                if(r_Digit_2_val >= 4'd9)
+                begin
+                    r_Digit_2_val <= 4'd0;
+                    if(r_Digit_1_val >= 4'd9)
+                        r_Digit_1_val <= 4'd0;
+                    else
+                        r_Digit_1_val <= r_Digit_1_val + 4'd1;
+                end
+                else
+                    r_Digit_2_val <= r_Digit_2_val + 4'd1;
+            end
+            else
+                r_Digit_3_val <= r_Digit_3_val + 4'd1;  
+        end
         else
-            r_Digit_1_val <= r_Digit_1_val + 1'b1;
-    end
-    
-    always@(posedge w_SUBCLK_10HZ)
-    begin
-        if(r_Digit_2_val == 4'hA)
-            r_Digit_2_val <=4'd0;
-        else
-            r_Digit_2_val <= r_Digit_2_val + 1'b1;
-    end
-    
-    always@(posedge w_SUBCLK_100HZ)
-    begin
-    if(r_Digit_3_val == 4'hA)
-        r_Digit_3_val <=4'd0;
-    else
-        r_Digit_3_val <= r_Digit_3_val + 1'b1;
-    end
-    
-    always@(posedge w_SUBCLK_1KHZ)
-    begin
-        if(r_Digit_4_val == 4'hA)
-            r_Digit_4_val <=4'd0;
-        else
-            r_Digit_4_val <= r_Digit_4_val + 1'b1;
-    end
+            r_Digit_4_val <= r_Digit_4_val + 4'd1;             
+    end 
 
     
     
