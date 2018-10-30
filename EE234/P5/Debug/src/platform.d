92b6f965cac8233c9b2bc5dd9b457a2e39e296c3
Elliott Villars <eman13vil@gmail.com>
2018-10-29 21:44:11 -0700

    -

---
+++ EE234/P5/Debug/src/platform.d
@@ -0,0 +1,15 @@
+src/platform.o src/platform.o: ../src/platform.c \
+ ../../P5_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../P5_bsp/ps7_cortexa9_0/include/xparameters_ps.h \
+ ../../P5_bsp/ps7_cortexa9_0/include/xil_cache.h \
+ ../../P5_bsp/ps7_cortexa9_0/include/xil_types.h ../src/platform_config.h
+
+../../P5_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../P5_bsp/ps7_cortexa9_0/include/xparameters_ps.h:
+
+../../P5_bsp/ps7_cortexa9_0/include/xil_cache.h:
+
+../../P5_bsp/ps7_cortexa9_0/include/xil_types.h:
+
+../src/platform_config.h:
