5e425d003b77c99e2949b95b06bbf792114c62ee
Elliott Villars <eman13vil@gmail.com>
2018-11-06 00:56:09 -0800

    -

---
+++ Module_11/Module_11.srcs/sources_1/new/SerialAdder.v
@@ -0,0 +1,94 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 11/05/2018 11:04:01 PM
+// Design Name: 
+// Module Name: SerialAdder
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module SerialAdder(
+    input i_CLK,
+    input i_RST,
+    input i_A,
+    input i_B,
+    output o_Sum,
+    output o_Cout
+    );
+    
+    
+    localparam STATE_0 = 2'b00;
+    localparam STATE_1 = 2'b01;
+    localparam STATE_2 = 2'b10;
+    localparam STATE_3 = 2'b11;
+    
+    reg [1:0] r_PREV_STATE;
+    reg [1:0] r_NEXT_STATE;
+    
+    always@(r_PREV_STATE,i_A,i_B)
+    begin
+        case(r_PREV_STATE)  
+            STATE_0:
+                begin
+                    if(i_A == 1'b0 && i_B == 1'b0)
+                        r_NEXT_STATE = STATE_0;
+                    else if(i_A == 1'b1 && i_B == 1'b1)
+                        r_NEXT_STATE = STATE_2;
+                    else
+                        r_NEXT_STATE = STATE_1;    
+                end
+            STATE_1:
+                begin
+                    if(i_A == 1'b0 && i_B == 1'b0)
+                        r_NEXT_STATE = STATE_0;
+                    else if(i_A == 1'b1 && i_B == 1'b1)
+                        r_NEXT_STATE = STATE_2;
+                    else
+                        r_NEXT_STATE = STATE_1;    
+                end
+            STATE_2:
+                begin
+                    if(i_A == 1'b0 && i_B == 1'b0)
+                        r_NEXT_STATE = STATE_1;
+                    else if(i_A == 1'b1 && i_B == 1'b1)
+                        r_NEXT_STATE = STATE_3;
+                    else
+                        r_NEXT_STATE = STATE_2;    
+                end
+            STATE_3:
+                begin
+                    if(i_A == 1'b0 && i_B == 1'b0)
+                        r_NEXT_STATE = STATE_1;
+                    else if(i_A == 1'b1 && i_B == 1'b1)
+                        r_NEXT_STATE = STATE_3;
+                    else
+                        r_NEXT_STATE = STATE_2;    
+                end
+            default:
+                r_NEXT_STATE = STATE_0;
+
+        endcase
+    end
+    always@(posedge i_CLK,posedge i_RST)
+    begin
+        if(i_RST == 1'b1)
+            r_PREV_STATE <= STATE_0;
+        else   
+            r_PREV_STATE <= r_NEXT_STATE;
+    end
+    assign o_Sum = (r_PREV_STATE == STATE_1 || r_PREV_STATE == STATE_3 )? 1'b1:1'b0;
+    assign o_Cout = (r_PREV_STATE == STATE_2 || r_PREV_STATE == STATE_3 )? 1'b1:1'b0;
+endmodule
---
+++ Module_11/Module_11.srcs/sources_1/new/stopwatchFSM.v
@@ -0,0 +1,36 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 11/06/2018 12:44:02 AM
+// Design Name: 
+// Module Name: stopwatchFSM
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module stopwatchFSM(
+    input i_Start,
+    input i_Stop,
+    input i_Inc,
+    input i_CLK1KHZ
+    
+    );
+    
+    
+    reg [1:0] r_CURRENT_STATE;
+    reg [1:0] r_NEXT_STATE;
+    
+    always
+endmodule
