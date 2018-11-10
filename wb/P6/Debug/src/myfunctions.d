54504429267087bfcfc25b8be4770519ef8bfc2b
Elliott Villars <eman13vil@gmail.com>
2018-11-10 11:47:26 -0800

    UART IN PROGRESS

---
+++ wb/P6/Debug/src/myfunctions.d
@@ -0,0 +1,23 @@
+src/myfunctions.o src/myfunctions.o: ../src/myfunctions.c \
+ ../src/myfunctions.h ../src/platform.h ../src/platform_config.h \
+ ../../P6_bsp/ps7_cortexa9_0/include/xil_printf.h \
+ ../../P6_bsp/ps7_cortexa9_0/include/xil_types.h \
+ ../../P6_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../P6_bsp/ps7_cortexa9_0/include/xparameters_ps.h \
+ ../../P6_bsp/ps7_cortexa9_0/include/bspconfig.h
+
+../src/myfunctions.h:
+
+../src/platform.h:
+
+../src/platform_config.h:
+
+../../P6_bsp/ps7_cortexa9_0/include/xil_printf.h:
+
+../../P6_bsp/ps7_cortexa9_0/include/xil_types.h:
+
+../../P6_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../P6_bsp/ps7_cortexa9_0/include/xparameters_ps.h:
+
+../../P6_bsp/ps7_cortexa9_0/include/bspconfig.h:
---
+++ wb/P6/Debug/src/platform.d
@@ -0,0 +1,15 @@
+src/platform.o src/platform.o: ../src/platform.c \
+ ../../P6_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../P6_bsp/ps7_cortexa9_0/include/xparameters_ps.h \
+ ../../P6_bsp/ps7_cortexa9_0/include/xil_cache.h \
+ ../../P6_bsp/ps7_cortexa9_0/include/xil_types.h ../src/platform_config.h
+
+../../P6_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../P6_bsp/ps7_cortexa9_0/include/xparameters_ps.h:
+
+../../P6_bsp/ps7_cortexa9_0/include/xil_cache.h:
+
+../../P6_bsp/ps7_cortexa9_0/include/xil_types.h:
+
+../src/platform_config.h:
