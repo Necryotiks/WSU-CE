f808c01cdf4a92c978176f56842314dcb0cf0f9e
Elliott Villars <eman13vil@gmail.com>
2018-11-16 01:02:50 -0800

    -

--- Parts/SIPO.v
+++ Parts/SIPO.v
@@ -22,7 +22,6 @@
 
 module SIPO(
 input i_SDI,
-<<<<<<< HEAD
 input i_SFT,
 input i_CLK,
 output  [7:0] o_OUT
@@ -42,28 +41,4 @@ if(i_SFT == 1)
     r_OUT = r_OUT << 1'b1;
 end
 assign o_OUT = r_OUT;
-=======
-input i_RST,
-input i_CLK,
-output  [7:0] o_LED
-    );
-    
-reg [7:0] i;
-reg [27:0] counter1;
-wire Y1;
-always @ (posedge(i_CLK))
-begin
-	counter1 <= counter1 + 1;
-end
-
-assign Y1 = counter1[22];
-
-always@(posedge(Y1))
-begin
-i[0] = i_SDI; 
-if(i_RST == 1)  
-    i = i << 1'b1;
-end
-assign o_LED = i;
->>>>>>> 8c676840f2d6ed352e8a720fb7e570aabcf8c2b5
 endmodule
