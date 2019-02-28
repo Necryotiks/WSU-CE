68b7e2390b40cc81b8c4ac65c2752ac9b8159497
Elliott Villars <eman13vil@gmail.com>
2019-02-28 13:02:09 -0800

    update repos

--- EE324/Module_1/Module_1.srcs/sources_1/imports/new/stopwatch_ssd_driver.v
+++ EE324/Module_1/Module_1.srcs/sources_1/imports/new/stopwatch_ssd_driver.v
@@ -21,17 +21,18 @@
 
 
 module stopwatch_ssd_driver(
+(* X_INTERFACE_INFO = "xilinx.com:signal:clock:1.0 i_SUBCLK CLK" *)
     input i_SUBCLK, 
     input i_RST,
     input i_CLK_EN,
-        input i_SRST,
+    input i_SRST,
     output [3:0] o_Digit_1_val,
     output [3:0] o_Digit_2_val,
     output [3:0] o_Digit_3_val,
     output [3:0] o_Digit_4_val
     );
-       parameter c_HEX_DEC = 9;//Override with either 9 or 15 for decimal or hex mode respectively.
-       reg [3:0] r_HEX_DEC;
+       parameter DISPLAY_MODE = 9;//Override with either 9 or 15 for decimal or hex mode respectively.
+       reg [3:0] r_DISPLAY_MODE = 4'b0000;
        reg [3:0] r_Digit_1_val = 4'b0000;
        reg [3:0] r_Digit_2_val = 4'b0000;
        reg [3:0] r_Digit_3_val = 4'b0000;
@@ -52,7 +53,7 @@ module stopwatch_ssd_driver(
 
     always@(posedge w_SUBCLK,posedge w_RST) 
     begin
-        r_HEX_DEC <= c_HEX_DEC; //verilog bullshit.
+        r_DISPLAY_MODE <= DISPLAY_MODE; //verilog bullshit.
          
         if(w_RST == 1'b1 | w_SRST == 1'b1)
             begin
@@ -64,16 +65,16 @@ module stopwatch_ssd_driver(
         else
             begin
           if(w_CLK_EN == 1'd1) begin 
-                if(r_Digit_4_val >= r_HEX_DEC)
+                if(r_Digit_4_val >= r_DISPLAY_MODE)
                     begin
                         r_Digit_4_val <= 4'd0;
-                        if(r_Digit_3_val >= r_HEX_DEC)
+                        if(r_Digit_3_val >= r_DISPLAY_MODE)
                             begin
                                 r_Digit_3_val <= 4'd0;
-                                if(r_Digit_2_val >= r_HEX_DEC)
+                                if(r_Digit_2_val >= r_DISPLAY_MODE)
                                     begin
                                         r_Digit_2_val <= 4'd0;
-                                        if(r_Digit_1_val >= r_HEX_DEC)
+                                        if(r_Digit_1_val >= r_DISPLAY_MODE)
                                             r_Digit_1_val <= 4'd0;
                                         else
                                             r_Digit_1_val <= r_Digit_1_val + 4'd1;
---
+++ Parts/VGA_controller.v
@@ -0,0 +1,127 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 11/23/2018 10:44:21 PM
+// Design Name: 
+// Module Name: VGA_controller
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module VGA_controller(
+    input i_CLK,
+    output o_HSYNC,
+    output o_VSYNC,
+    output o_VDE,
+    output [9:0] o_X_COORD,
+    output [9:0] o_Y_COORD
+    );
+    
+    
+    parameter v_END_OF_LINE = 800;
+    parameter v_END_OF_SCREEN = 525;
+    parameter v_HA_START = 0;
+    parameter v_VA_START = 0;
+    parameter v_HA_END = 640;
+    parameter v_VA_END = 480;
+    wire w_CLK;
+    reg r_HSYNC = 1'd0;
+    reg r_VSYNC = 1'd0; 
+    reg [9:0] r_HCNT = 10'd0;
+    reg [9:0] r_VCNT = 10'd0;
+    reg [9:0] r_X_COORD = 10'd0;
+    reg [9:0] r_Y_COORD = 10'd0;
+    reg r_VDE = 1'd0;
+    
+    assign w_CLK = i_CLK;
+    assign o_HSYNC = r_HSYNC;
+    assign o_VSYNC = r_VSYNC;
+    assign o_VDE = r_VDE;
+    assign o_X_COORD = r_X_COORD;
+    assign o_Y_COORD = r_Y_COORD;
+     
+    //assign o_X_COORD = (r_HCNT >= v_HA_START) ? (r_HCNT - v_HA_START) : 0; 
+   // assign o_Y_COORD = (r_VCNT >= v_VA_END) ? (v_VA_END - 1) : r_VCNT; 
+    //https://timetoexplore.net/blog/arty-fpga-vga-verilog-01
+    
+    
+    always@(posedge w_CLK)
+    begin
+        if((r_HCNT >= v_HA_START) && (r_HCNT < v_HA_END)) 
+            begin
+            r_X_COORD = r_HCNT;
+            end
+        else
+            r_X_COORD = 10'd0;
+    end
+    
+    always@(posedge w_CLK)
+    begin
+        if(r_VCNT >= v_VA_END)
+            r_Y_COORD = v_VA_END - 1'b1;
+        else
+            r_Y_COORD = r_VCNT;
+    end
+    
+    always@(posedge w_CLK) //PIXEL COUNT
+    begin
+        if(r_HCNT == v_END_OF_LINE) begin
+            r_HCNT = 10'd0;
+            end
+        else
+            begin
+            r_HCNT = r_HCNT + 1'b1;
+            end
+    end
+    
+    always@(posedge w_CLK) //LINE COUNT
+        begin
+            if(r_VCNT == v_END_OF_SCREEN) begin
+                r_VCNT = 10'd0;
+                end
+            else if (r_HCNT == v_END_OF_LINE)
+                begin
+                r_VCNT = r_VCNT + 1'b1;
+                end
+           else
+                begin
+                r_VCNT = r_VCNT;
+                end
+        end
+        
+        always@(posedge w_CLK)
+        begin
+            if((r_HCNT > v_HA_END + 16) && (r_HCNT <= v_HA_END + 16 + 96))
+                r_HSYNC = 1'b0;
+            else
+                r_HSYNC = 1'b1;
+        end
+        
+        always@(posedge w_CLK)
+        begin
+            if((r_VCNT > v_VA_END + 10 ) && (r_VCNT <= v_VA_END + 10 + 2))
+                r_VSYNC = 1'b0;
+            else
+                r_VSYNC = 1'b1;
+        end
+        
+        always@(posedge w_CLK)
+        begin
+            if((r_HCNT <= v_HA_END) && (r_VCNT <= v_VA_END))
+                r_VDE = 1'b1;
+            else
+                r_VDE = 1'b0;
+        end
+endmodule
